77abd3f28496dc61d1cab354340073a238089afb
Md Jewel Rana <mdjewelrana6487@gmail.com>
2022-01-24 23:49:53 +0600

    otp added

--- src/Component/Login/Login.js
+++ src/Component/Login/Login.js
@@ -16,18 +16,16 @@ function Login() {
   const [message, setMessage] = useState("")
   const [signInUser , setSingInUser] = useState({})
   const [imgUrl, setImgUrl] =useState("") 
+  const [quality, setQuality] = useState(false)
   const {name, } =signInUser
-  console.log("this is name" , name);
-  console.log( "img url found",imgUrl);
-  console.log(signInUser , "sign in user is found");
+  // console.log(signInUser , "sign in user is found");
   const [userLoggedIn, setUserLoggedIn] = useContext(userContext)
   
   console.log("user context is ready", userLoggedIn.email);
   const navigate = useNavigate()
   const location = useLocation()
 
-  console.log(navigate, "navigate is here");
-  console.log(location, "location is here");
+  // console.log(location, "location is here");
   let { from } = location.state || { from: { pathname: "/" } };
 
  
@@ -39,126 +37,132 @@ function Login() {
   const onSubmit = (data) => {
     console.log(data)
     setSingInUser(data)
+    
+    const {fastName,lastName, email,department,designation,otp} = data;
+    if (department == "Quality") {
+      setQuality(true)
+    }
+    else{
 
-    const {fastName,lastName, email,department,designation} = data;
-
+      setQuality(false)
+    }
     // create user email and password
-    if (
-      newUser &&
-      data.email &&
-      data.password &&
-      data.password === data.confirmPass
-    ) {
-      alert("data is ready");
-      firebase.auth().createUserWithEmailAndPassword(data.email, data.password)
-      .then((result) => {
-        // Signed in
-        const user = result.user;
-      setUserLoggedIn(user)
-      localStorage.setItem("user", user.email)
-      updateUserInfo(data.fastName)
-  //  user send to data base
-      if (user) {
-        fetch("https://lingerie.herokuapp.com/user",{
-          method:"POST",
-          headers:{"content-type":"application/json"},
-          body:JSON.stringify({name:fastName,lastName:lastName,department: department, designation:designation, imgUrl:imgUrl, email:email})
-        })
-        .then((response) => {
-          alert("user sign in successfully")
+//     if (
+//       newUser &&
+//       data.email &&
+//       data.password &&
+//       data.password === data.confirmPass
+//     ) {
+//       alert("data is ready");
+//       firebase.auth().createUserWithEmailAndPassword(data.email, data.password)
+//       .then((result) => {
+//         // Signed in
+//         const user = result.user;
+//       setUserLoggedIn(user)
+//       localStorage.setItem("user", user.email)
+//       updateUserInfo(data.fastName)
+//   //  user send to data base
+//       if (user) {
+//         fetch("https://lingerie.herokuapp.com/user",{
+//           method:"POST",
+//           headers:{"content-type":"application/json"},
+//           body:JSON.stringify({name:fastName,lastName:lastName,department: department, designation:designation, imgUrl:imgUrl, email:email})
+//         })
+//         .then((response) => {
+//           alert("user sign in successfully")
 
-          if (response) {
-            navigate(from)
-          }
+//           if (response) {
+//             navigate(from)
+//           }
           
-        })
+//         })
        
-      }
-      console.log(user.name);
-        // ...
-      })
-      .catch((error) => {
-        var errorMessage = error.message;
-        setMessage(errorMessage)
-        // ..
-      });
-    }
+//       }
+//       console.log(user.name);
+//         // ...
+//       })
+//       .catch((error) => {
+//         var errorMessage = error.message;
+//         setMessage(errorMessage)
+//         // ..
+//       });
+//     }
    
-// sign in with email and password
+// // sign in with email and password
 
-    if (!newUser && data.email && data.password) {
-      firebase.auth().signInWithEmailAndPassword(data.email, data.password)
-  .then((res) => {
-      const user= res.user
-      const message = "LoggedIn successfully"
-      setMessage(message)
-      setUserLoggedIn(user)
-      userInfoToken()
-      localStorage.setItem("user", user.email)
-     navigate(from)
-    // ...
-  })
-  .catch((error) => {
-    var errorMessage = error.message;
-    console.log(error.message)
-    setMessage(errorMessage);
-  });
+//     if (!newUser && data.email && data.password) {
+//       firebase.auth().signInWithEmailAndPassword(data.email, data.password)
+//   .then((res) => {
+//       const user= res.user
+//       const message = "LoggedIn successfully"
+//       setMessage(message)
+//       setUserLoggedIn(user)
+//       userInfoToken()
+//       localStorage.setItem("user", user.email)
+//      navigate(from)
+//     // ...
+//   })
+//   .catch((error) => {
+//     var errorMessage = error.message;
+//     console.log(error.message)
+//     setMessage(errorMessage);
+//   });
       
-    }
+//     }
 
-  };
+//   };
 
-// update user info
+// // update user info
 
-const updateUserInfo = (userName) => {
+// const updateUserInfo = (userName) => {
   
-const user = firebase.auth().currentUser;
+// const user = firebase.auth().currentUser;
 
-user.updateProfile({
-  displayName: userName
+// user.updateProfile({
+//   displayName: userName
   
-}).then(() => {
-  console.log("user name update successfully");
-  // Update successful
-  // ...
-}).catch((error) => {
-  console.log(error);
-  // An error occurred
-  // ...
-});  
+// }).then(() => {
+//   console.log("user name update successfully");
+//   // Update successful
+//   // ...
+// }).catch((error) => {
+//   console.log(error);
+//   // An error occurred
+//   // ...
+// });  
 
-}
+// }
 
-// verify id token
-const userInfoToken = () => {
-  firebase.auth().currentUser.getIdToken(true).then(function(idToken) {
-    localStorage.setItem("token", idToken)
-    console.log(idToken);
+// // verify id token
+// const userInfoToken = () => {
+//   firebase.auth().currentUser.getIdToken(true).then(function(idToken) {
+//     localStorage.setItem("token", idToken)
+//     console.log(idToken);
   
-  })
-  .catch(function(error) {
-    console.log(error);
-  })
+//   })
+//   .catch(function(error) {
+//     console.log(error);
+//   })
 }
 
 
 
 // img upload 
 const handleImg=(e) => {
-  console.log(e.target.files[0],"this is img data ");
-  const profileImg = e.target.files[0]
-  const imgInfo = new FormData()
-  imgInfo.set("key", "5bce158fb0a8dfa35765ad620a59622c")
-  imgInfo.append("image", profileImg)
-  axios.post("https://api.imgbb.com/1/upload", imgInfo)
-  .then(function (response) {
-    console.log(response.data.data.display_url);
-    const url = response.data.data.display_url
-    setImgUrl(url)
-  })
-  .catch(function(err) {
-    console.log(err);
-  })
+  // console.log(e.target.files[0],"this is img data ");
+  // const profileImg = e.target.files[0]
+  // const imgInfo = new FormData()
+  // imgInfo.set("key", "5bce158fb0a8dfa35765ad620a59622c")
+  // imgInfo.append("image", profileImg)
+  // axios.post("https://api.imgbb.com/1/upload", imgInfo)
+  // .then(function (response) {
+  //   console.log(response.data.data.display_url);
+  //   const url = response.data.data.display_url
+  //   setImgUrl(url)
+  // })
+  // .catch(function(err) {
+  //   console.log(err);
+  // })
 
  
 }
@@ -249,13 +253,32 @@ const handleImg=(e) => {
                       </span>
                     )}
                     <br />
-                    <input
+                    {/* <input
                       className="w-100 text-white rounded-3 bg-transparent border border-secondary"
                       placeholder="Department"
                       {...register("department", { required: true })}
-                    />
+                    /> */}
+                    <label >Department</label>
+                      <select {...register("department", { required: true })} className="w-100 text-white rounded-3 bg-transparent border border-secondary">
+                        <option value="Quality">Quality</option>
+                        <option value="Kc Print ltd.">Production</option>
+                        <option value="Management">Management</option>
+                        <option value="Others">Others</option>
+                      </select>
                     <br />
-                   
+                   {
+                     quality ?<input
+                     className="w-100 text-white rounded-3 bg-transparent border border-secondary"
+                     style={{ padding: "  7px 10px" }}
+                     placeholder="Your OTP Code Here"
+                     {...register("otp", { required: true })}
+                   />:""
+                   }
+                    {errors.otp && (
+                    <span style={{color: "red", fontSize:"18px" }}>
+                    **
+                    </span>
+                  )}
                   </div>
                   <div className="">
                   {errors.designation && (
@@ -329,19 +352,7 @@ const handleImg=(e) => {
                       </span>
                     )}
                     <br />
-                    {/* <input
-                      placeholder="Upload Your Image"
-                      // onChange={handleImg}
-                      type="file"
-                      id="uploadImg"
-                      style={{
-                        width: "25%",
-                        background: "grey",
-                        display: "none",
-                      }}
-                      className="text-white"
-                      {...register("img", { required: true })}
-                    /> */}
+                 
                     <label htmlFor="uploadImg" className="uploadBtn">
                       <span className="uploadImgIcon">
                    
