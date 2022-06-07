6febb83e7c194f997cdb44116cf89423fcf000e2
jewelran <mdjewelrana6487@gmail.com>
2022-06-07 23:49:50 +0600

    added side bar

--- src/Component/Admin/MaintainUserInfo/SubscriberInfo.js
+++ src/Component/Admin/MaintainUserInfo/SubscriberInfo.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react'
 import Navigation from './../../Navigation/Navigation';
-import { Link } from 'react-router-dom';
 import "../AdminPanel.css"
 import { useEffect } from 'react';
+import Sidebar from './Sidebar';
 function SubscriberInfo() {
   const [subscribe, setSubscribe] = useState([]);
   useEffect(()=> {
@@ -18,13 +18,8 @@ function SubscriberInfo() {
         <div style={{height:"100vh",background: "#004"}} className=' '>
         <Navigation></Navigation>
      <div className="container d-flex justify-content-center mt-4">
-       <div style={{background:"#002",maxHeight:"500px"}} className="col-md-3 ">
-         <ul>
-         <Link to = "/allUser"> <li style={{color:"white"}}>All User</li></Link>
-         <Link to ="/subscriber"><li> All Subscriber </li></Link>
-         <Link to ="/message"><li> Message info </li></Link>
-         <Link to ="/addNotification"> <li>Add Notification</li></Link>
-         </ul>
+     <div className="">
+         <Sidebar></Sidebar>
        </div>
        <div className="col-md-9">
      <div className=" d-flex justify-content-around">
